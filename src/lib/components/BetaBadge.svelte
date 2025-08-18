97c2104d28021086081d623dc672fe894b054ac6
louiscollinsjr <louiscollinsjr@gmail.com>
2025-08-18 15:40:53 +0300

    style: update beta badge UI with improved spacing and rounded corners

--- src/lib/components/BetaBadge.svelte
+++ src/lib/components/BetaBadge.svelte
@@ -3,5 +3,5 @@
 </script>
  
 <div class="text-center text-sm text-black/80 opacity-50 font-aeonik tracking-wide mt-0">
-  Now in <span class="inline-block rounded-sm border border-black/80 uppercase leading-20 px-2 mx-1 text-[11px]">Beta</span> with global TA teams.
+  Now in <span class="inline-flex items-center rounded-lg border border-black/80 uppercase leading-none px-3 py-[4px] mx-1 text-[11px]">Beta</span> with global TA teams.
 </div>
